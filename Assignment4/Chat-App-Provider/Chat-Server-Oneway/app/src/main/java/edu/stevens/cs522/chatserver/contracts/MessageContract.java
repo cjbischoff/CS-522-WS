f2732a186f5d4d686789351a45cb5746f5cf2447
Christopher.Bischoff <christopherjbischoff@gmail.com>
2019-09-23 13:44:03 -0600

    [create/assignment4] Completed Message/Peer entity

--- Assignment4/Chat-App-Provider/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/contracts/MessageContract.java
+++ Assignment4/Chat-App-Provider/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/contracts/MessageContract.java
@@ -34,6 +34,8 @@ public class MessageContract extends BaseContract {
 
     public static final String SENDER = "sender";
 
+    public static final String SENDER_ID = "sender_id";
+
     // TODO remaining columns in Messages table
 
     private int messageTextColumn = -1;
@@ -50,4 +52,52 @@ public class MessageContract extends BaseContract {
     }
 
     // TODO remaining getter and putter operations for other columns
+
+    private static int timestampColumn = -1;
+
+    public static long getTimestamp(Cursor cursor) {
+        if (timestampColumn < 0) {
+            timestampColumn = cursor.getColumnIndexOrThrow(TIMESTAMP);
+        }
+        return cursor.getLong(timestampColumn);
+    }
+
+    public static void putTimestamp(ContentValues out, Long timestamp) {
+        out.put(TIMESTAMP, timestamp);
+    }
+
+    private static int senderColumn = -1;
+
+    public static String getSender(Cursor cursor) {
+        if (senderColumn < 0) {
+            senderColumn = cursor.getColumnIndexOrThrow(SENDER);
+        }
+        return cursor.getString(senderColumn);
+    }
+
+    public static void putSender(ContentValues out, String sender) {
+        out.put(SENDER, sender);
+    }
+
+    private static int idColumn = -1;
+
+    public static long getId(Cursor cursor) {
+        if (idColumn < 0) {
+            idColumn = cursor.getColumnIndexOrThrow(_ID);
+        }
+        return Long.parseLong(cursor.getString(idColumn));
+    }
+
+    private static int senderIdColumn = -1;
+
+    public static long getSenderId(Cursor cursor) {
+        if (senderIdColumn < 0) {
+            senderIdColumn = cursor.getColumnIndexOrThrow(SENDER_ID);
+        }
+        return Long.parseLong(cursor.getString(senderIdColumn));
+    }
+
+    public static void putSenderId(ContentValues out, long senderId) {
+        out.put(SENDER_ID, senderId);
+    }
 }
--- Assignment4/Chat-App-Provider/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/contracts/PeerContract.java
+++ Assignment4/Chat-App-Provider/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/contracts/PeerContract.java
@@ -1,5 +1,7 @@
 package edu.stevens.cs522.chatserver.contracts;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -29,5 +31,61 @@ public class PeerContract extends BaseContract {
 
 
     // TODO define column names, getters for cursors, setters for contentvalues
+
+    public static final String NAME = "name";
+
+    public static final String TIMESTAMP = "timestamp";
+
+    public static final String ADDRESS = "address";
+
+    public static final String ID = "id";
+
+    private static int nameColumn = -1;
+
+    public static String getName(Cursor cursor) {
+        if (nameColumn < 0) {
+            nameColumn = cursor.getColumnIndexOrThrow(NAME);
+        }
+        return cursor.getString(nameColumn);
+    }
+
+    public static void putName(ContentValues out, String name) {
+        out.put(NAME, name);
+    }
+
+    private static int timestampColumn = -1;
+
+    public static long getTimestamp(Cursor cursor) {
+        if (timestampColumn < 0) {
+            timestampColumn = cursor.getColumnIndexOrThrow(TIMESTAMP);
+        }
+        return cursor.getLong(timestampColumn);
+    }
+
+    public static void putTimestamp(ContentValues out, long timestamp) {
+        out.put(TIMESTAMP, timestamp);
+    }
+
+    private static int addressColumn = -1;
+
+    public static byte[] getAddress(Cursor cursor) {
+        if (addressColumn < 0) {
+            addressColumn = cursor.getColumnIndexOrThrow(ADDRESS);
+        }
+        return cursor.getBlob(addressColumn);
+    }
+
+    public static void putAddress(ContentValues out, byte[] address) {
+        out.put(ADDRESS, address);
+    }
+
+    private static int idColumn = -1;
+
+    public static long getId(Cursor cursor) {
+        if (idColumn < 0) {
+            idColumn = cursor.getColumnIndexOrThrow(_ID);
+        }
+        return Long.parseLong(cursor.getString(idColumn));
+    }
 
 }
