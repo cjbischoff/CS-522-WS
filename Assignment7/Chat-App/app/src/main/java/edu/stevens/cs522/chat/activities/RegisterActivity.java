489d59dded8dd93425ec071e533160a9d5f9c687
Christopher.Bischoff <christopherjbischoff@gmail.com>
2019-11-08 08:43:18 -0700

    [create/assignment7] Initial code from class

---
+++ Assignment7/Chat-App/app/src/main/java/edu/stevens/cs522/chat/activities/RegisterActivity.java
@@ -0,0 +1,120 @@
+/*********************************************************************
+
+    Chat server: accept chat messages from clients.
+    
+    Sender chatName and GPS coordinates are encoded
+    in the messages, and stripped off upon receipt.
+
+    Copyright (c) 2017 Stevens Institute of Technology
+
+**********************************************************************/
+package edu.stevens.cs522.chat.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.stevens.cs522.chat.R;
+import edu.stevens.cs522.chat.rest.ChatHelper;
+import edu.stevens.cs522.chat.settings.Settings;
+import edu.stevens.cs522.chat.services.ResultReceiverWrapper;
+
+public class RegisterActivity extends Activity implements OnClickListener, ResultReceiverWrapper.IReceive {
+
+	final static public String TAG = RegisterActivity.class.getCanonicalName();
+		
+    /*
+     * Widgets for dest address, message text, send button.
+     */
+    private EditText userNameText;
+
+    private Button registerButton;
+
+    /*
+     * Helper for Web service
+     */
+    private ChatHelper helper;
+
+    /*
+     * For receiving ack when registered.
+     */
+    private ResultReceiverWrapper registerResultReceiver;
+	
+	/*
+	 * Called when the activity is first created. 
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        /**
+         * Initialize settings to default values.
+         */
+		if (Settings.isRegistered(this)) {
+			finish();
+            return;
+		}
+
+        setContentView(R.layout.register);
+
+        // TODO instantiate helper for service
+
+
+        // TODO initialize registerResultReceiver
+
+
+        userNameText = (EditText) findViewById(R.id.chat_name_text);
+
+        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(this);
+
+    }
+
+	public void onResume() {
+        super.onResume();
+        registerResultReceiver.setReceiver(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        registerResultReceiver.setReceiver(null);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    /*
+     * Callback for the REGISTER button.
+     */
+    public void onClick(View v) {
+        if (!Settings.isRegistered(this) && helper != null) {
+
+            String userName = userNameText.getText().toString();
+
+            // TODO use helper to register
+
+
+            // End todo
+
+        }
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle data) {
+        switch (resultCode) {
+            case RESULT_OK:
+                Toast.makeText(this, "Registered!", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                Toast.makeText(this, "Failed to register!", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+}
